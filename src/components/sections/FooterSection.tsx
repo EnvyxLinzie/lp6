e8ed0f961f1deba894aae081924a598773371d90
chyp <help@chyp.dev>
2024-08-05 08:57:06 +0700

    first commit

--- src/components/sections/FooterSection.tsx
+++ src/components/sections/FooterSection.tsx
@@ -37,10 +37,10 @@ function FooterSection() {
             Contact Us:
           </div>
 
-          <p className="text-white mt-[27px]">Email: info@positivus.com</p>
-          <p className="text-white mt-[27px]">Phone: 555-567-8901</p>
+          <p className="text-white mt-[27px]">Email: info@capscyber.com</p>
+          <p className="text-white mt-[27px]">Phone: +628513289xxxx</p>
           <p className="text-white mt-[27px]">
-            Address: 1234 Main St Moonstone City, Stardust State 12345
+            Address: Purwokerto, Indonesia
           </p>
         </div>
         <div className="bg-[#292A32] flex flex-col md:flex-row gap-4 justify-center items-center md:gap-[20px] py-16 px-[40px] rounded-[14px]">
@@ -53,7 +53,7 @@ function FooterSection() {
       </div>
       <div className="flex flex-col md:flex-row gap-1 md:gap-[40px]">
         <p className="text-white text-p">
-          © 2023 Positivus. All Rights Reserved.
+          © 2024 Positivus. All Rights Reserved.
         </p>
         <p className="text-white underline text-p">Privacy Policy</p>
       </div>
